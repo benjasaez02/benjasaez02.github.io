bbd1c65e2e4c9e8246eac48028733064f8ab473a
ofertasflashcl <118039594+ofertasflashcl@users.noreply.github.com>
2026-07-28 16:02:37 -0400

    Add scalable offer data registry

---
+++ offers-data.js
@@ -0,0 +1,29 @@
+window.OFFERS = [
+  {
+    id: 'poco-c85',
+    category: 'Tecnología',
+    featured: true,
+    active: true,
+    brand: 'Xiaomi POCO',
+    name: 'POCO C85',
+    variant: 'Negro · 128 GB · 6 GB RAM',
+    title: 'Xiaomi POCO C85 128 GB + 6 GB RAM',
+    shortDescription: 'Pantalla de hasta 120 Hz, batería de 6000 mAh y una combinación de memoria difícil de encontrar por este precio.',
+    price: 79990,
+    oldPrice: null,
+    currency: 'CLP',
+    badge: 'Oferta destacada',
+    score: 9.4,
+    image: 'https://i02.appmifile.com/250_operator_sg/22/08/2025/4420debe3086c2ddb343e53c537d5153.png',
+    imageFallback: 'assets/phone-hero.svg',
+    specs: [
+      { label: 'Almacenamiento', value: '128 GB' },
+      { label: 'Memoria RAM', value: '6 GB' },
+      { label: 'Batería', value: '6000 mAh' },
+      { label: 'Pantalla', value: 'Hasta 120 Hz' }
+    ],
+    webUrl: 'https://www.mercadolibre.cl/xiaomi-poco-c85-negro-128-gb-rom-6-gb-ram/p/MLC54097181?matt_event_ts=1785261105007&matt_d2id=ab94ecd3-be74-48e1-ad95-463ebea22d08&matt_tracing_id=6dea6bb4-2893-454d-9f13-e79af508cd5c#polycard_client=recommendations_home_affiliate-profile&reco_backend=item_decorator&reco_client=home_affiliate-profile&matt_tool_id=82210482&reco_item_pos=0&source=affiliate-profile&reco_backend_type=function&reco_id=6d544f66-fe66-4a9c-adc3-f38d46a55db7&tracking_id=6f35980a-b107-4a40-bbee-5c247beb79c3&c_uid=23354150-4fc6-460f-b273-b90050f76a59&c_id=%2Fhome%2Fcard-featured%2Felement',
+    affiliateUrl: 'https://ddnf.adj.st/webview/?url=https%3A%2F%2Fwww.mercadolibre.cl%2Fxiaomi-poco-c85-negro-128-gb-rom-6-gb-ram%2Fp%2FMLC54097181%3Fmatt_event_ts%3D1785261105007%26matt_d2id%3Dab94ecd3-be74-48e1-ad95-463ebea22d08%26matt_tracing_id%3D6dea6bb4-2893-454d-9f13-e79af508cd5c%23polycard_client%3Drecommendations_home_affiliate-profile%26reco_backend%3Ditem_decorator%26reco_client%3Dhome_affiliate-profile%26matt_tool_id%3D82210482%26reco_item_pos%3D0%26source%3Daffiliate-profile%26reco_backend_type%3Dfunction%26reco_id%3D6d544f66-fe66-4a9c-adc3-f38d46a55db7%26tracking_id%3D6f35980a-b107-4a40-bbee-5c247beb79c3%26c_uid%3D23354150-4fc6-460f-b273-b90050f76a59%26c_id%3D%252Fhome%252Fcard-featured%252Felement&adj_campaign=social&adj_t=1y8rwb1z',
+    publishedAt: '2026-07-28'
+  }
+];
