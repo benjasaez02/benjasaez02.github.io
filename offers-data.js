aaa3119218a82b3717632db815dc4e08aa12d7df
ofertasflashcl <118039594+ofertasflashcl@users.noreply.github.com>
2026-07-28 16:43:00 -0400

    Add Moto G86 Power affiliate offer

--- offers-data.js
+++ offers-data.js
@@ -25,5 +25,32 @@ window.OFFERS = [
     webUrl: 'https://www.mercadolibre.cl/xiaomi-poco-c85-negro-128-gb-rom-6-gb-ram/p/MLC54097181?matt_event_ts=1785261105007&matt_d2id=ab94ecd3-be74-48e1-ad95-463ebea22d08&matt_tracing_id=6dea6bb4-2893-454d-9f13-e79af508cd5c#polycard_client=recommendations_home_affiliate-profile&reco_backend=item_decorator&reco_client=home_affiliate-profile&matt_tool_id=82210482&reco_item_pos=0&source=affiliate-profile&reco_backend_type=function&reco_id=6d544f66-fe66-4a9c-adc3-f38d46a55db7&tracking_id=6f35980a-b107-4a40-bbee-5c247beb79c3&c_uid=23354150-4fc6-460f-b273-b90050f76a59&c_id=%2Fhome%2Fcard-featured%2Felement',
     affiliateUrl: 'https://ddnf.adj.st/webview/?url=https%3A%2F%2Fwww.mercadolibre.cl%2Fxiaomi-poco-c85-negro-128-gb-rom-6-gb-ram%2Fp%2FMLC54097181%3Fmatt_event_ts%3D1785261105007%26matt_d2id%3Dab94ecd3-be74-48e1-ad95-463ebea22d08%26matt_tracing_id%3D6dea6bb4-2893-454d-9f13-e79af508cd5c%23polycard_client%3Drecommendations_home_affiliate-profile%26reco_backend%3Ditem_decorator%26reco_client%3Dhome_affiliate-profile%26matt_tool_id%3D82210482%26reco_item_pos%3D0%26source%3Daffiliate-profile%26reco_backend_type%3Dfunction%26reco_id%3D6d544f66-fe66-4a9c-adc3-f38d46a55db7%26tracking_id%3D6f35980a-b107-4a40-bbee-5c247beb79c3%26c_uid%3D23354150-4fc6-460f-b273-b90050f76a59%26c_id%3D%252Fhome%252Fcard-featured%252Felement&adj_campaign=social&adj_t=1y8rwb1z',
     publishedAt: '2026-07-28'
+  },
+  {
+    id: 'moto-g86-power-5g',
+    category: 'Tecnología',
+    featured: false,
+    active: true,
+    brand: 'Motorola',
+    name: 'Moto G86 Power 5G',
+    variant: 'Azul · 256 GB · 8 GB RAM',
+    title: 'Motorola Moto G86 Power 5G 8 + 256 GB',
+    shortDescription: 'Gama media 5G con pantalla pOLED Super HD de hasta 120 Hz, batería enorme de 6720 mAh y resistencia IP68/IP69.',
+    price: 219990,
+    oldPrice: 399990,
+    currency: 'CLP',
+    badge: '45% OFF · Más vendido',
+    score: 9.3,
+    image: 'https://p1-ofp.static.pub/fes/cms/2026/01/11/ytg3dvz4ffxyl60w5xwa0iqvr9fxf2180163.png',
+    imageFallback: 'assets/phone-hero.svg',
+    specs: [
+      { label: 'Almacenamiento', value: '256 GB' },
+      { label: 'Memoria RAM', value: '8 GB' },
+      { label: 'Batería', value: '6720 mAh' },
+      { label: 'Conectividad', value: '5G + NFC' }
+    ],
+    webUrl: 'https://www.mercadolibre.cl/moto-g86-power-5g-8256gb-azul/p/MLC66215014?matt_event_ts=1785271034805&matt_d2id=b0d24616-0224-4af0-b834-16606ba9c785&matt_tracing_id=2fe53a06-79a9-4080-99bf-c4abba9d0a4c#polycard_client=recommendations_home_affiliate-profile&reco_backend=item_decorator&reco_client=home_affiliate-profile&matt_tool_id=82210482&reco_item_pos=0&source=affiliate-profile&reco_backend_type=function&reco_id=3bd5be5b-9cc5-46ff-9972-e32dd9631711&tracking_id=33037b4f-c36c-4225-ad7e-06b4294b3ea2&c_uid=260d896b-65b1-4c1f-88fd-c8c1ffccf3cd&c_id=%2Fhome%2Fcard-featured%2Felement',
+    affiliateUrl: 'https://ddnf.adj.st/webview/?url=https%3A%2F%2Fwww.mercadolibre.cl%2Fmoto-g86-power-5g-8256gb-azul%2Fp%2FMLC66215014%3Fmatt_event_ts%3D1785271034805%26matt_d2id%3Db0d24616-0224-4af0-b834-16606ba9c785%26matt_tracing_id%3D2fe53a06-79a9-4080-99bf-c4abba9d0a4c%23polycard_client%3Drecommendations_home_affiliate-profile%26reco_backend%3Ditem_decorator%26reco_client%3Dhome_affiliate-profile%26matt_tool_id%3D82210482%26reco_item_pos%3D0%26source%3Daffiliate-profile%26reco_backend_type%3Dfunction%26reco_id%3D3bd5be5b-9cc5-46ff-9972-e32dd9631711%26tracking_id%3D33037b4f-c36c-4225-ad7e-06b4294b3ea2%26c_uid%3D260d896b-65b1-4c1f-88fd-c8c1ffccf3cd%26c_id%3D%252Fhome%252Fcard-featured%252Felement&adj_campaign=social&adj_t=1y8rwb1z',
+    publishedAt: '2026-07-28'
   }
 ];
